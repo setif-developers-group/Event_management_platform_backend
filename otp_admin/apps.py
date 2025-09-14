9092cc6590379a3ef83ea5b1b6f861022b6fb7d9
aymen merad <ay28mene@gmail.com>
2025-09-14 22:12:05 +0100

    fix bug

--- otp_admin/apps.py
+++ otp_admin/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
+
 from decouple import config
-def create_admin():
+from django.db.models.signals import post_migrate
+def create_admin(sender, **kwargs):
     '''
     Create admin user if not exists
     '''
+    from django.contrib.auth import get_user_model
     User = get_user_model()
     if not User.objects.filter(username=config('ADMIN_USERNAME', 'aymen')).exists():
         User.objects.create_superuser(username=config('ADMIN_USERNAME','aymen'), email=config('ADMIN_EMAIL','aymen_merad@proton.me'), password=config('ADMIN_PASSWORD','admin'))
@@ -15,4 +17,4 @@ class OtpAdminConfig(AppConfig):
     name = 'otp_admin'
     def ready(self):
         import otp_admin.signals
-        create_admin()
+        post_migrate.connect(create_admin, sender=self)
